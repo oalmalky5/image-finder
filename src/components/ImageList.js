00a666224dcb8e6ee06af4f02bef5ceb311de1bb
oalmalky5 <oalmalky5@gmail.com>
2020-09-12 18:22:05 +0300

    adding style and finishing touches

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './ImageList.css'
+import ImageCard from './ImageCard'
 
 const ImageList = (props) => {
 
-    const images1  = props.images.map( ({discription, id, urls}) => {
-        return <img key={id} src={urls.regular} alt={discription} />
+    const images1  = props.images.map( (image) => {
+        return <ImageCard key={image.id}  image={image} />
     })
 
     return <div className='image-list'>{images1}</div>
